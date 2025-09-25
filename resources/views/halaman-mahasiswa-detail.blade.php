5552813e68355c630a9e1650818670e41272c98c
Endy <endy24ti@mahasiswa.pcr.ac.id>
2025-09-25 09:22:27 +0700

    nmax dxika

--- resources/views/halaman-mahasiswa-detail.blade.php
+++ resources/views/halaman-mahasiswa-detail.blade.php
@@ -1,6 +1,6 @@
 <html>
     <body>
         <h1>Selamat Datang!</h1>
-        <h2>Ini halaman Detail Mahasiswa ofjrdsoigmjroig</h2>
+        <h2>Ini halaman Detail Mahasiswa jhsjsh</h2>
     </body>
 </html>l
