005ae6b626d47c35c3b2ee01ef80d817ead8ce8a
endy24ti <endy24ti@mahasiswa.pcr.ac.id>
2025-10-16 10:28:46 +0700

    Modulp6

--- routes/web.php
+++ routes/web.php
@@ -8,6 +8,8 @@ use App\Http\Controllers\QuestionController;
 use App\Http\Controllers\HomeController;
 use App\Http\Controllers\DashboardController;
 
+use App\Http\Controllers\PelangganController;
+
 Route::get('/', function () {
     return view('welcome');
 });
@@ -41,3 +43,5 @@ Route::get('/matakuliah/show/{param1}', [MatakuliahController::class, 'show']);
 Route::post('question/store', [QuestionController::class, 'store'])->name('question.store');
 Route::get('/home', [HomeController::class, 'index'])->name('home');
 Route::get('dashboard', [DashboardController::class, 'index'])->name('dashboard');
+
+Route::resource('pelanggan', PelangganController::class);
